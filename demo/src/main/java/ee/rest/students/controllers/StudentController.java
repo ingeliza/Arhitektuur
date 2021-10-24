e1e94cdbf0ae070b6a2a97b79d38ff16cea84275
Ingela <59916738+JoinEliza@users.noreply.github.com>
2021-10-24 13:07:38 +0300

    added students

---
+++ demo/src/main/java/ee/rest/students/controllers/StudentController.java
@@ -0,0 +1,26 @@
+
+public class StudentController {
+    @GetMapping("/students")
+    public List<Student> findAll(){
+        return service.mockStudents();
+    }
+
+    @GetMapping("/students/{id}")
+    Student oneStudent(@PathVariable int id) {
+        return service.mockOneStudent(id);
+    }
+
+    @PostMapping("/students")
+    Student newStudent(@RequestBody Student newStudent){
+        return null;
+    }
+
+    @PutMapping("/students/{id}")
+    Student updateStudent(@RequestBody Student newStudent, @PathVariable int id) {
+        return null;
+    }
+
+    @DeleteMapping("/students/{id}")
+    void deleteStudent(@PathVariable int id) {
+    }
+}
---
+++ demo/src/main/java/ee/rest/students/dto/Student.java
@@ -0,0 +1,5 @@
+public class Student {
+    String studentName {get; set;}
+    String subject {get; set;}
+    List<int> grades {get; set;}
+}
